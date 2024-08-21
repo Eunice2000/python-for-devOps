7ff0448545d11c59e9c554ba2fbe9d3dc32ac093
Eunice2000 <adediraneunice20@gmail.com>
2024-08-21 23:30:28 +0100

    Day-one

--- DAY-01/main.py
+++ DAY-01/main.py
@@ -1,1 +1,6 @@
-print("welcome to python for devOps from Zero to Hero!")
+print ('welcome to python for devOps from Zero to Hero!')
+
+print("no more time for procastination it is time to learn")
+
+
+print("i am back with bunch energy")
