80e4a4cdcec4f5d4715466a4398b74be71ce553b
Eunice2000 <adediraneunice20@gmail.com>
2024-08-22 01:11:10 +0100

    Day-two

---
+++ DAY-02/test2.py
@@ -0,0 +1,71 @@
+ARN = "                      123456798/EUNICE"
+# Test = ARN.strip()
+# print (Test)
+print (ARN.lower().strip())
+print (len(ARN.split("/")[1]))  ## string inbuilt functions using split and lower
+
+
+FirstName = "eunice"
+LastName = "Adebukola"
+
+FullName = FirstName + " " + LastName
+
+print(FullName)
+
+## MATCH
+
+import re
+
+text = "today is sucessful"
+
+pattern = r"today1"
+
+search = re.match( pattern, text)
+
+if search:
+    print("that exist")
+else:
+    print("none")
+
+
+
+## REPLACE
+
+Caption = "No more procastination today"
+
+
+replace = r"today"
+
+ToThis = "this week"
+
+Complet = re.sub(replace, ToThis, Caption)
+
+
+if Complet:
+    print(Complet)
+
+else:
+    print("none of it")
+
+
+## SEARCH
+
+
+Searching = "eunice" "bukky" "tope"
+
+Check = r"tope"
+
+replace = "john"
+
+# if Check:
+#     print( re.search(Check, Searching).group(), "is found and it is being replace with", 
+#           re.sub(Check, replace, Searching).replace('"', '').split())
+if re.search(Check, Searching):
+    found = re.search(Check, Searching).group()
+    replaced_string = re.sub(Check, replace, Searching)
+    # Split the string into a list
+    result_list = replaced_string.replace('"', '').split(" , ")
+    
+    print(f"{found} is found and it is being replaced with", result_list)
+else:
+    print("not found")
